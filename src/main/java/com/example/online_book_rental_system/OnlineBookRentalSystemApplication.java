f62153cb41fd6ade7f05cf981a2240e68603715f
Abhisek Dash <abhisekdash6931@gmail.com>
2024-12-02 23:43:33 +0530

    first commit

---
+++ src/main/java/com/example/online_book_rental_system/OnlineBookRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_book_rental_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookRentalSystemApplication.class, args);
+	}
+
+}
